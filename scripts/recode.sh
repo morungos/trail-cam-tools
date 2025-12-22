34ade2f783045bef6ed340a989b2af0d13ff2bb2
Stuart Watt <morungos@gmail.com>
2025-12-22 17:51:00 -0400

    basic file processing completed

--- scripts/recode.sh
+++ scripts/recode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 ## This script takes an AVI file that is on the trail camera's SD card, 
 ## splits the audio and the video using `gpac`, and then remuxes them
@@ -12,25 +13,52 @@
 
 INROOT=$1
 OUTROOT=$2
-FILENAME=$3
 
-## Make sure the output directory exists
+recode_file(){
+  INROOT=$1
+  OUTROOT=$2
+  FILENAME=$3
 
-INFILE="${INROOT}/${FILENAME}"
-OUTFILE="${OUTROOT}/${FILENAME}"
-OUTDIR="$(dirname "$OUTFILE")"
 
-mkdir -p "${OUTDIR}"
+  ## Make sure the output directory exists
 
-## Pull apart the streams
-gpac -i "${INFILE}" -o "${OUTFILE}.pcm" -o "${OUTFILE}.h264"
+  INFILE="${INROOT}/${FILENAME}"
 
-## mux them back, encoding the audio
-ffmpeg -y \
-  -f s16le -ar 16000 -ac 1 -i "${OUTFILE}.pcm" \
-  -i "${OUTFILE}.h264" \
-  -c:v copy -c:a aac \
-  "${OUTFILE}"
+  INFILE_EXT="${INFILE##*.}"
+  INFILE_TYPE=`file -b --mime-type "${INFILE}"`
 
-## and remove the temporary files
-rm "${OUTFILE}.pcm" "${OUTFILE}.h264"
+  if [[ "${INFILE_TYPE}" == "image/jpeg" ]]; then
+    OUTFILE="${OUTROOT}/${FILENAME}"
+    OUTDIR="$(dirname "$OUTFILE")"
+    echo "Copying: ${INFILE} to ${OUTFILE}"
+    cp "${INFILE}" "${OUTFILE}"
+    return
+  fi
+
+  if [[ "${INFILE_TYPE}" == "video/x-msvideo" ]]; then
+    OUTFILE="${OUTROOT}/${FILENAME%.*}.MP4"
+    OUTDIR="$(dirname "$OUTFILE")"
+
+    echo "Re-muxing video: ${INFILE} to ${OUTFILE}"
+
+    mkdir -p "${OUTDIR}"
+
+    ## Pull apart the streams
+    gpac -i "${INFILE}" -o "${OUTFILE}.pcm" -o "${OUTFILE}.h264"
+
+    ## mux them back, encoding the audio
+    ffmpeg -y \
+      -f s16le -ar 16000 -ac 1 -i "${OUTFILE}.pcm" \
+      -i "${OUTFILE}.h264" \
+      -c:v copy -c:a aac \
+      "${OUTFILE}"
+
+    ## and remove the temporary files
+    rm "${OUTFILE}.pcm" 
+    rm "${OUTFILE}.h264"
+    return
+  fi
+}
+
+export -f recode_file
+find "$INROOT" -type f -printf "%P\n" | xargs -I{} bash -c 'recode_file "$@"' recode_file "$INROOT" "$OUTROOT" "{}"
