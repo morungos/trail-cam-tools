29e826c415e1c5be95dcdafab1c24130b8de8b56
Stuart Watt <stuart@morungos.com>
2026-06-13 21:40:08 -0300

    started work on date extraction

---
+++ scripts/get-image-text.applescript
@@ -0,0 +1,72 @@
+-- Command line version
+-- usage:
+-- osascript extract_text_from_image_live_text_ocr.applescript ~/Desktop/test/*.gif
+
+use framework "Vision"
+use framework "Foundation"
+use scripting additions -- For file operations
+
+on run argv
+
+    set { imagePath } to { item 1 } of argv
+
+    -- Convert file path to POSIX path
+    set imagePath to POSIX path of imagePath
+    
+    -- Ensure the file exists before attempting to process
+    if not (do shell script "test -f " & quoted form of imagePath & "; echo $?") = "0" then
+        log "Error: File does not exist at path: " & imagePath
+        error "File does not exist at path: " & imagePath
+    end if
+    
+    -- Convert to NSURL
+    set fileURL to (current application's NSURL's fileURLWithPath:imagePath)
+    
+    -- Get image content
+    set theImage to (current application's NSImage's alloc()'s initWithContentsOfURL:fileURL)
+    
+    if theImage is missing value then
+        log "Error: Image could not be loaded at path: " & imagePath
+        error "Image could not be loaded at path: " & imagePath
+    end if
+    
+    -- Set up request handler using image's raw data
+    set imageData to theImage's TIFFRepresentation()
+    if imageData is missing value then
+        log "Error: Failed to extract TIFF representation for image: " & imagePath
+        error "Failed to extract TIFF representation."
+    end if
+    
+    set requestHandler to (current application's VNImageRequestHandler's alloc()'s initWithData:imageData options:(current application's NSDictionary's alloc()'s init()))
+    
+    -- Initialize text request
+    set theRequest to current application's VNRecognizeTextRequest's alloc()'s init()
+    
+    -- Perform the request and get the results
+    (requestHandler's performRequests:(current application's NSArray's arrayWithObject:(theRequest)) |error|:(missing value))
+    set theResults to theRequest's results()
+    
+    -- Obtain and return the string values of the results
+    set theText to ""
+    repeat with observation in theResults
+        set recognizedText to ((first item in (observation's topCandidates:1))'s |string|() as text)
+        set theText to theText & recognizedText & linefeed
+    end repeat
+    
+    -- Get the directory and filename of the image
+    -- set imageDirectory to (do shell script "dirname " & quoted form of imagePath)
+    -- set imageFileNameWithExtension to (do shell script "basename " & quoted form of imagePath)
+    
+    -- Create the output path with the same filename but .text-data extension
+    -- set outputPath to imageDirectory & "/" & imageFileNameWithExtension & ".text-data"
+    
+    -- Save the recognized text to a file in the same folder as the image
+    -- set fileRef to open for access (outputPath as POSIX file) with write permission
+    -- write theText to fileRef
+    -- close access fileRef
+    
+    -- Log the successful processing
+    copy theText to stdout
+
+end run
+
--- scripts/recode.sh
+++ scripts/recode.sh
@@ -17,7 +17,15 @@ set -x
 INROOT=$1
 OUTROOT=$2
 
-update_timestamps(){
+SCRIPT_DIR=$(dirname "$0")
+echo "SCRIPT_DIR" $SCRIPT_DIR
+
+## On a Mac, the simplest way to get the time from an image and/or video
+## is to have an image and extract the text. This is guaranteed to be 
+## correct, and not depending on whether or not the original file times
+## were sound.
+
+update_video_timestamps(){
   INFILE=$1
   OUTFILE=$2
 
@@ -41,30 +49,85 @@ update_timestamps(){
     "${OUTFILE}"
 }
 
+## Reads the timestamp from the image, which should be interpreted as local time
+## although we may or may not handle daylight savings.
+
+get_timestamp() {
+  FILENAME=$1
+
+  IMAGE_TEXT=`osascript $SCRIPT_DIR/get-image-text.applescript "$FILENAME"`
+  echo "Image file data:" $IMAGE_TEXT
+
+  DATE_REGEX='([0-3][0-9])/([0-1][0-9])/([2][0-9][0-9][0-9]) ([0-2][0-9]):([0-5][0-9]):([0-5][0-9])'
+
+  if [[ $IMAGE_TEXT =~ $DATE_REGEX ]]; then
+    echo "Date: ${BASH_REMATCH[1]}/${BASH_REMATCH[2]}/${BASH_REMATCH[3]}"
+  else
+    echo "No match"
+  fi
+}
+
+update_image_timestamps(){
+  INFILE=$1
+  OUTFILE=$2
+
+  # we really want to get text from the file
+  # IMAGE_TEXT=`osascript $SCRIPT_DIR/get-image-text.applescript "$INFILE"`
+  # echo "Image file data:" $IMAGE_TEXT
+
+  get_timestamp "${INFILE}"
+
+  # # extract the modification time, which seems correct, and reformat to UTC rfc-3339
+  # MTIME_UTC=`stat --format "%Y" "${INFILE}"`
+  # FORMATTED_MTIME_UTC=`date --rfc-3339=seconds -u -d "@${MTIME_UTC}"`
+
+  # TAG_MTIME_UTC_I1="${FORMATTED_MTIME_UTC//-/:}"
+  # TAG_MTIME_UTC="${TAG_MTIME_UTC_I1/%+00:00/:}"
+
+  # exiftool -overwrite_original -api QuickTimeUTC=1 -tagsFromFile "${INFILE}" \
+  #   '-OffsetTimeOriginal=+00:00' \
+  #   '-OffsetTimeDigitized=+00:00' \
+  #   "-DateTimeOriginal=${TAG_MTIME_UTC}" \
+  #   "-CreateDate=${TAG_MTIME_UTC}" \
+  #   "-MediaCreateDate=${TAG_MTIME_UTC}" \
+  #   "-TrackCreateDate=${TAG_MTIME_UTC}" \
+  #   "-ModifyDate=${TAG_MTIME_UTC}" \
+  #   "-MediaModifyDate=${TAG_MTIME_UTC}" \
+  #   "-TrackModifyDate=${TAG_MTIME_UTC}" \
+  #   "${OUTFILE}"
+}
+
 recode_file(){
-  INROOT=$1
-  OUTROOT=$2
-  FILENAME=$3
+  FILENAME=$1
 
   ## Make sure the output directory exists
 
-  INFILE="${INROOT}/${FILENAME}"
+  echo "INROOT" $INROOT
+  INFILE="${FILENAME#$INROOT/}"
+  echo "FILENAME" $FILENAME
+  echo "INFILE" $INFILE
 
   INFILE_EXT="${INFILE##*.}"
-  INFILE_TYPE=`file -b --mime-type "${INFILE}"`
+  INFILE_TYPE=`file -b --mime-type "$INROOT/${INFILE}"`
+
+  echo "INFILE_TYPE" $INFILE_TYPE
 
   ## JPEG files, copy across directly
   if [[ "${INFILE_TYPE}" == "image/jpeg" ]]; then
-    OUTFILE="${OUTROOT}/${FILENAME}"
+    OUTFILE="${OUTROOT}/${INFILE}"
     OUTDIR="$(dirname "$OUTFILE")"
-    echo "Copying: ${INFILE} to ${OUTFILE}"
-    cp "${INFILE}" "${OUTFILE}"
-    update_timestamps "${INFILE}" "${OUTFILE}"
+    mkdir -p "${OUTDIR}"
+    echo "Copying: $INROOT/${INFILE} to ${OUTFILE}"
+    cp "$INROOT/${INFILE}" "${OUTFILE}"
+    update_image_timestamps "$INROOT/${INFILE}" "${OUTFILE}"
     return
   fi
 
+  return
+
   ## AVI files, extract the audio and video and re-multiplex
   if [[ "${INFILE_TYPE}" == "video/x-msvideo" ]]; then
+    return
     OUTFILE="${OUTROOT}/${FILENAME%.*}.MP4"
     OUTDIR="$(dirname "$OUTFILE")"
 
@@ -85,11 +148,17 @@ recode_file(){
     ## and remove the temporary files
     rm "${OUTFILE}.pcm" 
     rm "${OUTFILE}.h264"
-    update_timestamps "${INFILE}" "${OUTFILE}"
+    update_video_timestamps "${INFILE}" "${OUTFILE}"
     return
   fi
 }
 
+export -f get_timestamp
 export -f recode_file
-export -f update_timestamps
-find "$INROOT" -type f -printf "%P\0" | sort -z | xargs -0 -I{} bash -c 'recode_file "$@"' recode_file "$INROOT" "$OUTROOT" "{}"
+export -f update_video_timestamps
+export -f update_image_timestamps
+export SCRIPT_DIR
+export INROOT
+export OUTROOT
+
+find "$INROOT" -type f -print0 | sort -z | xargs -0 -I{} bash -c 'recode_file "{}"' 
