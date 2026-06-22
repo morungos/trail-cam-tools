e94b6de5bf2c95fb0cd5b68a44eb07c1cac3577b
Stuart Watt <stuart@morungos.com>
2026-06-22 12:33:39 -0300

    started adding text cache and command args

--- src/sync.ts
+++ src/sync.ts
@@ -3,19 +3,38 @@ import path from 'path';
 
 import mime from 'mime-types'
 
-import type { ConfigData, Context } from './types.d.ts';
+import type { ConfigData, Context } from './types.d.ts'
 
 import logger from './logger'
 
+import { program } from 'commander'
+import { Level } from 'level'
+
+program
+    .argument('<source>')
+    .argument('<target>')
+
+// For persistence, we will use level. That's enough: all we need
+// is a key value store with a JSON data block containing a timestamp
+// and extracted (raw) text. Particularly for videos, this is much more
+// efficient than re-extracting the text every single time.
+
 function makeContext(): Context {
+
+    program.parse()
+    const cache = new Level('.text-cache', { valueEncoding: 'json' })
+
     return {
         config: {
-            source: "",
-            target: ""
+            source: program.args[0],
+            target: program.args[1],
+            cache: cache
         }
     };
 }
 
+// The main top level function, which iterates through directories 
+// searching for data.
 async function processContent(ctx: Context) {
     const directory = ctx.config.source
     const files = await fs.readdir(directory, {
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,9 @@
+import type { Level } from 'level'
+
 export type ConfigData = {
     source: string,
     target: string,
+    cache: Level
 }
 
 export type Context = {
